1a9ba1bdf1cfaf52c58e57fed1357262b2b3f32b
Maksim Stogniy <mstogniy@ya.ru>
2024-06-09 21:00:36 +0400

    Update requirements, project structure

---
+++ rps-game-ios/Modules/Rules/RulesView.swift
@@ -0,0 +1,45 @@
+//
+//  RulesView.swift
+//  rps-game-ios
+//
+//  Created by  Maksim Stogniy on 09.06.2024.
+//
+
+import UIKit
+
+final class RulesView: UIView {
+    private var title: UILabel = {
+        let label = UILabel()
+        label.text = "Rules"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setViews()
+        layoutViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setViews()
+        layoutViews()
+    }
+    
+    func setViews() {
+        backgroundColor = .white
+        [
+            title
+        ].forEach { addSubview($0) }
+    }
+    
+    func layoutViews() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            title.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            title.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+    }
+}
