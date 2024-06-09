05bc71060afa762ff98ce99ef8b0201c3e08caa4
Maksim Stogniy <mstogniy@ya.ru>
2024-06-09 15:35:54 +0400

    Init

---
+++ rps-game-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rps-game-ios
+//
+//  Created by  Maksim Stogniy on 09.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
