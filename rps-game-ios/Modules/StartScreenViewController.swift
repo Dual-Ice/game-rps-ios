0a03bd3211fcf3463e2663007659b718ea292b87
Bokhulenkov <bokhulenkov@gmail.com>
2024-06-10 17:59:30 +0300

    creating and configuring start button

--- rps-game-ios/Modules/StartScreenViewController.swift
+++ rps-game-ios/Modules/StartScreenViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class StartScreenViewController: UIViewController {
+final class StartScreenViewController: UIViewController {
+    
+    private let startButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("START", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Rubik", size: 16)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(#colorLiteral(red: 0.7684260011, green: 0.5614033341, blue: 0.4590145946, alpha: 1), for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.backgroundColor = #colorLiteral(red: 0.9843137255, green: 0.7647058824, blue: 0.6, alpha: 1)
+        button.layer.cornerRadius = 20
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createUI()
 
     }
     
+    //    MARK: - Selectors
+        
+        @objc func startGame() {
+            print("Start Game")
+        }
+    
+    //    MARK: - Helpers
+    
+    private func createUI() {
+        view.backgroundColor = #colorLiteral(red: 0.9607843137, green: 0.968627451, blue: 0.9843137255, alpha: 1)
+        
+        view.addSubview(startButton)
+        startButtonConstraints()
+        startButton.addTarget(self, action: #selector(startGame), for: .touchUpInside)
+        
+        
+    }
+    
+    func startButtonConstraints() {
+        NSLayoutConstraint.activate([
+            startButton.widthAnchor.constraint(equalToConstant: 196),
+            startButton.heightAnchor.constraint(equalToConstant: 53),
+            startButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 622),
+            startButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 90.5)
+        ])
+    }
 
     
 }
