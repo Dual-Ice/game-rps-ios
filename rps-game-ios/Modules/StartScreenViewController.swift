4679f2a851baa27751108c282ea101bc43cf27d2
Bokhulenkov <bokhulenkov@gmail.com>
2024-06-10 17:52:29 +0300

    creating StartScreenViewController and changing SceneDelegate

---
+++ rps-game-ios/Modules/StartScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StartScreenViewController.swift
+//  rps-game-ios
+//
+//  Created by Alexander Bokhulenkov on 10.06.2024.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
