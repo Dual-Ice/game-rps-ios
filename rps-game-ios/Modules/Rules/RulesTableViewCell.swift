0411121d170d3279bb2ffe6d109f75e491efde4f
Bokhulenkov <bokhulenkov@gmail.com>
2024-06-11 19:26:42 +0300

    creating rule table view cell

---
+++ rps-game-ios/Modules/Rules/RulesTableViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  RuleTableViewCell.swift
+//  rps-game-ios
+//
+//  Created by Alexander Bokhulenkov on 11.06.2024.
+//
+
+import UIKit
+
+class RulesTableViewCell: UITableViewCell {
+
+//    инконки кулак, бумага, ножницы
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let numberLebel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .yellow
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 10
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(iconImageView)
+        contentView.addSubview(numberLebel)
+        contentView.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+                    numberLebel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+                    numberLebel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                    numberLebel.widthAnchor.constraint(equalToConstant: 30),
+                    numberLebel.heightAnchor.constraint(equalToConstant: 30),
+                    
+                    iconImageView.leadingAnchor.constraint(equalTo: numberLebel.trailingAnchor, constant: 10),
+                    iconImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                    iconImageView.widthAnchor.constraint(equalToConstant: 30),
+                    iconImageView.heightAnchor.constraint(equalToConstant: 30),
+                    
+                    descriptionLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 10),
+                    descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+                    descriptionLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+                ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    конфигурация ячейки
+    func configureCell(number: Int, description: String, icon: UIImage?) {
+        numberLebel.text = "\(number)"
+        descriptionLabel.text = description
+        iconImageView.image = icon
+    }
+}
--- rps-game-ios/Modules/Rules/RulesView.swift
+++ rps-game-ios/Modules/Rules/RulesView.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 final class RulesView: UIView {
+    
+    private let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    
+    private let cellIdentifier = "RulesCell"
+    
+    private let rules = [
+            ("Игра проводится между игроком и компьютером.", nil),
+            ("Жесты:", nil),
+            ("Кулак > Ножницы", UIImage(named: "rules")),
+            ("Бумага > Кулак", UIImage(named: "rules")),
+            ("Ножницы > Бумага", UIImage(named: "rules")),
+            ("У игрока есть 30 сек. для выбора жеста.", nil),
+            ("Игра ведётся до трёх побед одного из участников.", nil),
+            ("За каждую победу игрок получает 500 баллов, которые можно посмотреть на доске лидеров.", nil)
+        ]
+    
     private var title: UILabel = {
         let label = UILabel()
         label.text = "Rules"
@@ -30,8 +46,14 @@ final class RulesView: UIView {
     func setViews() {
         backgroundColor = .white
         [
-            title
+            title,
+            tableView
         ].forEach { addSubview($0) }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(RulesTableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
     }
     
     func layoutViews() {
@@ -40,7 +62,28 @@ final class RulesView: UIView {
         
         NSLayoutConstraint.activate([
             title.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
-            title.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            title.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            tableView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            tableView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ])
     }
+}
+
+extension RulesView: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        rules.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier , for: indexPath) as? RulesTableViewCell else { return UITableViewCell() }
+        
+        let rule = rules[indexPath.row]
+        cell.configureCell(number: indexPath.row + 1, description: rule.0, icon: rule.1)
+        return cell
+    }
+    
+    
 }
