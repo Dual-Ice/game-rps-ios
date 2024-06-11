07c86bf8205647c7c3ab1fdc84ea2883ee966545
Bokhulenkov <bokhulenkov@gmail.com>
2024-06-11 09:43:31 +0300

    creating StartScreenView file

---
+++ rps-game-ios/Modules/StartScreen/StartScreenView.swift
@@ -0,0 +1,15 @@
+//
+//  StartScreenView.swift
+//  rps-game-ios
+//
+//  Created by Alexander Bokhulenkov on 11.06.2024.
+//
+
+import UIKit
+
+final class StartScreenView: UIView {
+    
+    
+    
+    
+}
