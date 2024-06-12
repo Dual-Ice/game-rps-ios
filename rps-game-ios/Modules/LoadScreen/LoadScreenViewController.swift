ed72aae8772d04b1e5b98789e766dfd5f049dcbb
Alexander <17aml08@gmail.com>
2024-06-12 14:20:09 +0300

    remove duplicate images

--- rps-game-ios/Modules/LoadScreen/LoadScreenViewController.swift
+++ rps-game-ios/Modules/LoadScreen/LoadScreenViewController.swift
@@ -20,15 +20,5 @@ class LoadScreenViewController: UIViewController {
         super.viewDidLoad()
 
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
