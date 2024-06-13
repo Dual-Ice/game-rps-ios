203bd4bf15efed6601d08fe424c3eef174f6fc98
Nikita Moskalev <nikmosyl@gmail.com>
2024-06-13 14:36:38 +0400

    refactor

--- rps-game-ios/Modules/GameScreen/GameScreenView.swift
+++ rps-game-ios/Modules/GameScreen/GameScreenView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol GameScreenViewDelegate: AnyObject {
     //Назад
+    
     //Пауза
     //Камень
     //Ножницы
--- rps-game-ios/Modules/GameScreen/GameScreenViewController.swift
+++ rps-game-ios/Modules/GameScreen/GameScreenViewController.swift
@@ -12,7 +12,6 @@ final class GameScreenViewController: UIViewController, GameScreenViewDelegate {
     private let gameScreenView = GameScreenView()
     
     override func loadView() {
-        //super.loadView() //?
         view = gameScreenView
     }
     
